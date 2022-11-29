03c2e572c686e98080cd2d5473003d6affb696fb
David <lim.david93@gmail.com>
2022-11-29 19:22:03 +1100

    Complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array)
+        array.select {|n| n.split("").sort == @name.split("").sort}
+    end
+
+end
